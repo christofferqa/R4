6efc115324c1e5f551d05c9488fe98417ae5ceea
Christoffer Quist Adamsen <c.adamsen@partner.samsung.com>
2016-04-28 10:12:37 -0700

    Proxy flag

--- run_browser.sh
+++ run_browser.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-WebKitBuild/Release/bin/QtTestBrowser $1
+
+SITE=""
+PROXY="false"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --h)
+            echo "Usage: ./run-browser.sh <website URL> [--proxy]"
+            exit 0
+            ;;
+        --proxy)
+            PROXY="true"
+            ;;
+        *)
+            if [[ $1 == --* ]]; then
+                echo "Invalid argument: $1."
+                exit 1
+            fi
+            SITE=$1
+            ;;
+    esac
+    shift
+done
+
+if [[ "$PROXY" == "true" ]] ; then
+    http_proxy=http://"":""@127.0.0.1:8081 WebKitBuild/Release/bin/QtTestBrowser $SITE
+else
+	WebKitBuild/Release/bin/QtTestBrowser $SITE
+fi
