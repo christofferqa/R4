da1388648a63750ccdbdeb87e4f47865f38bb4fb
Christoffer Quist Adamsen <c.adamsen@partner.samsung.com>
2016-05-04 17:28:30 -0700

    Better support for controlling output dirs

--- R4/r4.sh
+++ R4/r4.sh
@@ -4,16 +4,16 @@ RECORD=$WEBERA_DIR/R4/record.sh
 MC=$WEBERA_DIR/R4/model-check.sh
 
 if (( ! $# > 0 )); then
-    echo "Usage: ./r4.sh <website URL> [--manual] [--proxy] [--report] [--outdir=<dir>] [--trigger-event-type=<str> --trigger-node-identifier=<str>]"
-    echo "Outputs result to <outdir>/<website dir>"
+    echo "Usage: ./r4.sh <website URL> [--manual] [--proxy] [--report] [--rawdir=<dir>] [--reportdir=<dir>] [--trigger-event-type=<str> --trigger-node-identifier=<str>]"
     exit 1
 fi
 
 AUTO="--auto"
-DST_OUTPUT_DIR=output
 HIGH_TIME_LIMIT=""
 REPORT="false"
 PROXY=""
+RAWDIR=""
+REPORTDIR=""
 SITE=""
 TRIGGER_EVENT_TYPE=""
 TRIGGER_NODE_IDENTIFIER=""
@@ -21,7 +21,7 @@ TRIGGER_NODE_IDENTIFIER=""
 while [ $# -gt 0 ]; do
     case "$1" in
         --h)
-            echo "Usage: ./r4.sh <website URL> [--high-time-limit] [--manual] [--proxy] [--report] [--outdir=<dir>] [--trigger-event-type=<str> --trigger-node-identifier=<str>]"
+            echo "Usage: ./r4.sh <website URL> [--high-time-limit] [--manual] [--proxy] [--report] [--rawdir=<dir>] [--reportdir=<dir>] [--trigger-event-type=<str> --trigger-node-identifier=<str>]"
             echo "Example: ./r4.sh abc.xyz --report"
             echo "Example: ./r4.sh abc.xyz --report --trigger-event-type=click --trigger-node-identifier=\"https://abc.xyz/ @ #document[0]/HTML[6]/BODY[3]/MAIN[1]/DIV[7]/DIV[1]/DIV[5]/P[1]/A[3]\""
             echo ""
@@ -37,14 +37,18 @@ while [ $# -gt 0 ]; do
         --manual)
             AUTO=""
             ;;
-        --outdir=*)
-            DST_OUTPUT_DIR="${1#*=}"
+        --rawdir=*)
+            RAWDIR="${1#*=}"
             ;;
         --proxy)
             PROXY="--proxy"
             ;;
         --report)
             REPORT="true"
+            ;;
+        --reportdir=*)
+            REPORT="true"
+            REPORTDIR="${1#*=}"
             ;;
         --trigger-event-type=*)
             AUTO=""
@@ -55,7 +59,7 @@ while [ $# -gt 0 ]; do
             TRIGGER_NODE_IDENTIFIER="${1#*=}"
             ;;
         *)
-            if [[ $1 == --* ]]; then
+            if [[ $1 == --* ]] ; then
                 echo "Invalid argument: $1."
                 exit 1
             fi
@@ -68,53 +72,72 @@ done
 if [[ "$SITE" == "" ]] ; then
     echo "Site is mandatory"
     exit 1
-elif [[ $SITE == http* ]]; then
+elif [[ $SITE == http* ]] ; then
     echo "Site must not include protocol"
     exit 1
 fi
 
 ID=${SITE//[:\/\.]/\-}
 
-OUTDIR=~/r4tmp/$RANDOM
-while [ -f $OUTDIR ]; do
-    OUTDIR=~/r4tmp/$RANDOM
+DEFAULT_RAWDIR=output
+DEFAULT_REPORTDIR=report
+TMPDIR=~/r4tmp/$RANDOM
+while [ -f $TMPDIR ]; do
+    TMPDIR=~/r4tmp/$RANDOM
 done
-OUTRUNNER=$OUTDIR/runner
+OUTRUNNER=$TMPDIR/runner
 
-rm -rf $OUTDIR
+rm -rf $TMPDIR
 mkdir -p $OUTRUNNER
-mkdir -p $DST_OUTPUT_DIR
+mkdir -p $DEFAULT_RAWDIR
+mkdir -p $DEFAULT_REPORTDIR
 
-echo "Running " $SITE " @ " $OUTDIR
+echo "Running " $SITE " @ " $TMPDIR
 
-echo "Running CMD: $RECORD $SITE $OUTDIR $PROXY --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier=\"$TRIGGER_NODE_IDENTIFIER\" >> $OUTRUNNER/record.log 2>> $OUTRUNNER/record.log"
-$RECORD $SITE $OUTDIR $PROXY --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier="$TRIGGER_NODE_IDENTIFIER" >> $OUTRUNNER/record.log 2>> $OUTRUNNER/record.log
+ERROR=""
+
+echo "Running CMD: $RECORD $SITE $TMPDIR $PROXY --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier=\"$TRIGGER_NODE_IDENTIFIER\" >> $OUTRUNNER/record.log 2>> $OUTRUNNER/record.log"
+$RECORD $SITE $TMPDIR $PROXY --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier="$TRIGGER_NODE_IDENTIFIER" >> $OUTRUNNER/record.log 2>> $OUTRUNNER/record.log
 if [[ $? == 0 ]] ; then
     if [[ "$TRIGGER_EVENT_TYPE" != "" && "$TRIGGER_NODE_IDENTIFIER" != "" ]] ; then
-        cat $OUTDIR/record/out.log | grep -F "Warning: Node ($TRIGGER_NODE_IDENTIFIER) not found..."
+        cat $TMPDIR/record/out.log | grep -F "Warning: Node ($TRIGGER_NODE_IDENTIFIER) not found..."
         if [[ $? == 0 ]] ; then
             exit 1
         fi
     fi
 
-    echo "Running CMD: $MC $SITE $OUTDIR --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier=\"$TRIGGER_NODE_IDENTIFIER\" $HIGH_TIME_LIMIT --depth 1  >> $OUTRUNNER/mc.log 2>> $OUTRUNNER/mc.log"
-    $MC $SITE $OUTDIR --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier="$TRIGGER_NODE_IDENTIFIER" $HIGH_TIME_LIMIT --depth 1  >> $OUTRUNNER/mc.log 2>> $OUTRUNNER/mc.log
+    echo "Running CMD: $MC $SITE $TMPDIR --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier=\"$TRIGGER_NODE_IDENTIFIER\" $HIGH_TIME_LIMIT --depth 1  >> $OUTRUNNER/mc.log 2>> $OUTRUNNER/mc.log"
+    $MC $SITE $TMPDIR --verbose $AUTO $TRIGGER --trigger-event-type=$TRIGGER_EVENT_TYPE --trigger-node-identifier="$TRIGGER_NODE_IDENTIFIER" $HIGH_TIME_LIMIT --depth 1  >> $OUTRUNNER/mc.log 2>> $OUTRUNNER/mc.log
     if [[ ! $? == 0 ]] ; then
-        echo "model-checker errord out, see $DST_OUTPUT_DIR/$ID/runner/mc.log"
-    elif [ ! -d $OUTDIR/base ]; then
-        echo "Error: Repeating the initial recording failed"
+        ERROR="model-checker errord out, see $DEFAULT_RAWDIR/$ID/runner/mc.log"
+    elif [ ! -d $TMPDIR/base ] ; then
+        ERROR="Error: Repeating the initial recording failed"
     fi
-
 else
-    echo "Initial recording errord out, see $DST_OUTPUT_DIR/$ID/runner/record.log"
+    ERROR="Initial recording errord out, see $DEFAULT_RAWDIR/$ID/runner/record.log"
 fi
 
 rm -rf output/$ID
-mv $OUTDIR $DST_OUTPUT_DIR/$ID
+mv $TMPDIR $DEFAULT_RAWDIR/$ID
 
-if [[ "$REPORT" == "true" ]]; then
-    mkdir -p report
+if [[ "$ERROR" != "" ]] ; then
+    echo "$ERROR"
+    exit 1
+fi
 
-    echo "Running CMD: ./utils/batch-report/report.py \"website\" output report \"$ID\""
-    ./utils/batch-report/report.py "website" output report "$ID"
+if [[ "$REPORT" == "true" ]] ; then
+    echo "Running CMD: ./utils/batch-report/report.py \"website\" $DEFAULT_RAWDIR $DEFAULT_REPORTDIR \"$ID\""
+    ./utils/batch-report/report.py "website" $DEFAULT_RAWDIR $DEFAULT_REPORTDIR "$ID"
+fi
+
+if [[ "$RAWDIR" != "" ]] ; then
+    rm -rf $RAWDIR
+    mkdir -p $RAWDIR
+    mv $DEFAULT_RAWDIR/$ID/* $RAWDIR/
+fi
+
+if [[ "$REPORTDIR" != "" ]] ; then
+    rm -rf $REPORTDIR
+    mkdir -p $REPORTDIR
+    mv $DEFAULT_REPORTDIR/$ID/* $REPORTDIR/
 fi
